83d902fcb250eb24896bd7e88efeb514ffcbf54b
Eugenio <bravieugenio@gmail.com>
2022-03-06 20:43:23 -0300

    finish console.log() rock-paper-scissor game

--- js.js
+++ js.js
@@ -28,14 +28,39 @@ make the pc pick a random option
 const rock = "Rock";
 const paper = "Paper";
 const scissor  = "Scissor";
-let playerSelection = "";
-let computerSelection = "";
+const choises = ["Rock","Paper","Scissor"];
+//let playerSelection;
+//let computerSelection;
+//let computerSelection = choises[Math.floor(Math.random() * choises.length)] ;
 let playerScore = 0;
 let computerScore = 0;
-function game(playerSelection, computerSelection){
+let rounds = 0;
 
+function computerPlay() {
+  return choises[~~(Math.random() * choises.length)];
+}
+
+function game(){
+  for (let i = 0; i < 5; i++, rounds++) {
+  playRound()
+ }
+ if (rounds === 5){
+  endGame();
+ }
+}
+function endGame(){
+  if (playerScore>computerScore){
+    console.log("Player wins the game");
+  }else{
+    console.log("Computer wins the game");
+  }
+}
+
+function playRound(){
+let playerSelection = prompt("Choose Rock, Paper or Scissor",);
+let computerSelection = computerPlay();
   if (playerSelection === computerSelection){
-    return console.log("tie"), playerScore += 0.5 , computerScore += 0.5; 
+     console.log("tie"), playerScore += 0.5 , computerScore += 0.5; 
   }else if (playerSelection === "Rock"){
     if (computerSelection === "Scissor"){
       console.log("Player Wins"), playerScore += 1;
@@ -48,8 +73,8 @@ function game(playerSelection, computerSelection){
     }else{
       console.log("Computer Wins"), computerScore += 1;
     }
-  }else if(playerSelection === "Paper"){
-    if (computerSelection === "Rock"){
+  }else if(playerSelection === "Scissor"){
+    if (computerSelection === "Paper"){
       console.log("Player Wins"), playerScore += 1;
     }else{
       console.log("Computer Wins"), computerScore += 1;
